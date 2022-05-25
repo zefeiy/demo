d42ddb24584531277eb7db27899bd0a2e577fb66
zefeiy <1241808529@qq.com>
2022-05-25 21:56:50 +0800

    提交

---
+++ essential c++/第二章/2-1.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+using namespace std;
+
+bool fiebo_elem(int pos, unsigned int& elem);
+
+int main() {
+	int pos;
+	cout << "input pos:\n";
+	cin >> pos;
+	while (pos !=-1) {
+		
+		unsigned int elem = 0;
+		if (fiebo_elem(pos, elem)) {
+			cout << "index" << pos << "result is:" << elem << endl;
+			cout << "input pos:\n";
+			cin >> pos;
+		}
+		else {
+			cout << "pos is error\n";
+			cout << "input pos:\n";
+			cin >> pos;
+		}
+	}
+}
+bool fiebo_elem(int pos,unsigned int& elem) {
+	
+	int n1 = 1, n2 = 1;
+	elem = 1;
+	if (pos > 1024 || pos <= 0) {
+		return false;
+	}
+	for (int i = 3; i <= pos; i++) {
+		elem = n1 + n2;
+		n1 = n2;
+		n2 = elem;
+	}
+	return true;
+
+}
