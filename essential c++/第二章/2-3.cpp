00d21bf7bb531f811b40d11fcff71916c7cb013f
zefeiy <1241808529@qq.com>
2022-05-26 22:44:20 +0800

    提交

---
+++ essential c++/第二章/2-3.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void Pentagonal(int pos,vector<int>& ary) {
+	//static vector<int> ary;
+	for (int i = ary.size()+1; i <= pos; i++) {
+		ary.push_back(i*(3*i-1)/2);
+	}
+}
+
+inline bool is_ok(int pos,vector<int>& ary) {
+	if (pos >= 64 || pos <= 0 || pos<ary.size())
+		return false;
+	Pentagonal(pos,ary);
+	return true;
+}
+
+
+void printve(const vector<int>& ary,const string& str) {
+	cout << "print everyone element\n";
+	for (int i = 0; i < ary.size(); i++) {
+		cout << ary[i] << " ";
+	}
+	cout << endl;
+
+}
+
+int main() {
+	vector<int> ary;
+	const string ty("element type");
+	if (is_ok(5, ary))
+		printve(ary, ty);
+		//Pentagonal(5,ary);
+	return 0;
+}
---
+++ essential c++/第二章/2-4.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+
+const vector<int>* Pentagonal(int pos) {
+	static vector<int> ary;
+	if (pos < 0)
+		return NULL;
+	for (int i = ary.size() + 1; i <= pos; i++) {
+		ary.push_back(i*(3 * i - 1) / 2);
+	}
+	return &ary;
+}
+
+int find_element(int pos, vector<int> const *ary) {
+	if (ary == NULL || pos > ary->size())
+		return -1;
+	return (*ary)[pos];
+}
+
+
+int main() {
+	const vector<int>* ary = Pentagonal(10);
+	cout << find_element(5, ary);
+
+
+}
