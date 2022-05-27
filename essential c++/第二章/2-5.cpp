8063cb24ba7f3a182c9ed33134e5c17e8ebb68a0
zefeiy <1241808529@qq.com>
2022-05-27 14:30:15 +0800

    提交

---
+++ essential c++/第二章/2-5.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int max(int a, int b);
+double max(double a, double b);
+string& max(const string& a, const string& b);
+int max(const vector<int>& ary);
+double max(const vector<double>& ary)
+
+
+
+int max(int a,int b) {
+	return a > b ? a : b;
+}
+ 
+double max(double a, double b) {
+	cout << 123;
+	return a > b ? a : b;
+}
+
+string& max(string& a, string& b) {
+	return a.size() > b.size() ? a : b;
+}
+
+int max(const vector<int>& ary) {
+	int m = ary[0];
+	for (int i = 1; i < ary.size(); i++) {
+		if (ary[i] > m)
+			m = ary[i];
+	}
+	return m;
+}
+
+double max(const vector<double>& ary) {
+	int m = ary[0];
+	for (int i = 1; i < ary.size(); i++) {
+		if (ary[i] > m)
+			m = ary[i];
+	}
+	return m;
+}
+
+
+
+
+int main() {
+	int a = 10, b = 20;
+	double da = 10, db = 20;
+	//cout << max(a, b);
+	//cout << max(da, db);
+
+
+}
+
+
+
+
---
+++ essential c++/第二章/2-6.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+template<typename T>
+T max(T& a,T& b) {
+	return a > b ? a : b;
+}
+
+template<typename T>
+T max(T* a, T& b) {
+	T m = a[0];
+	for (int i = 0; i < b; i++) {
+		if (a[i] > m) m = a[i];
+	}
+	return m;
+}
+
+template<typename T>
+T max(vector<T>& ary) {
+	T m = ary[0];
+	for (int i = 0; i < ary.size(); i++) {
+		if (ary[i] > m) m = ary[i];
+	}
+	return m;
+}
+
+
+int main() {
+	string a = "";
+	string b = "sadas";
+	int ary[10] = { 2,3,4 };
+	int len=10;
+	vector<int> bary(ary, ary + 3);
+	//cout << max(ary, len);
+	cout << max(bary);
+	return 0;
+}
