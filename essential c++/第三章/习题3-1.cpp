05d1ecf4f95b09921ede0d1f107370a428291570
zefeiy <1241808529@qq.com>
2022-05-29 13:42:02 +0800

    提交

---
+++ essential c++/第三章/习题3-1.cpp
@@ -0,0 +1,52 @@
+﻿#include<iostream>
+#include<iterator>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<fstream>
+#include<map>
+#include<set>
+
+using namespace std;
+int main() {
+	/*
+	ifstream in_file("Text.txt");
+	ofstream out_file("sort_test.txt");
+
+	istream_iterator<string> is(in_file);
+	istream_iterator<string> eof;
+
+	vector<string> text;
+	copy(is, eof, back_inserter(text));
+	sort(text.begin(), text.end());
+
+	ostream_iterator<string> os(out_file, " ");
+	copy(text.begin(), text.end(), os);
+	*/
+
+	set<string> set({"a","an","or","the","and","but"});
+	map<string, int> mp;
+	ifstream in_file("Text.txt");
+	istream_iterator<string> is(in_file);
+	istream_iterator<string> eof;
+	
+	while (is != eof) {
+		if (!set.count(*is)) {
+			map<string, int>::iterator it;
+			it = mp.find(*is);
+			if (it == mp.end())
+				mp[*is]++;
+			else {
+				it->second++;
+			}
+		}
+		is++;
+	}
+
+	for (auto it = mp.begin(); it != mp.end(); ++it) {
+		//fprintf(stdout, it->first + "");
+		cout << it->first << ":" << it->second << endl;
+	}
+
+	return 0;
+}
---
+++ essential c++/第三章/习题3-2.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<iterator>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<fstream>
+using namespace std;
+
+
+int main() {
+	ifstream in_file("Text.txt");
+
+	istream_iterator<string> is(in_file);
+	istream_iterator<string> eof;
+	vector<string> vec;
+	copy(is,eof,back_inserter(vec));
+	
+	sort(vec.begin(), vec.end(), [](const string& a, const string& b) {
+		return a.size() < b.size();
+	});
+
+	for (vector<string>::const_iterator it = vec.begin(); it != vec.end(); ++it) {
+		cout << *it << " " << endl;
+	}
+
+	return 0;
+}
